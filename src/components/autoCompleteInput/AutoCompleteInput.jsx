80e8a6ca0147a2e038707f14ee498a5f3494125c
Fernando <ing.fernandovg@gmail.com>
2024-06-08 12:36:03 -0600

    create and apply handleChange from input

--- src/components/autoCompleteInput/AutoCompleteInput.jsx
+++ src/components/autoCompleteInput/AutoCompleteInput.jsx
@@ -6,10 +6,16 @@ const AutoCompleteInput = ({suggestions, onSelect}) => {
 
     const [inputValue, setInputValue] = useState('');
     const [filterSuggestions, setFilterSuggestions] = useState([]);
+
+    const handleChange = (e) => {
+        const value = e.target.value;
+        setInputValue(value);
+    };
+
     return (
         <div className="autocomplete">
-            <input type="text" value={inputValue} onChange={''} className='autocomplete__input' />
-            
+            <input type="text" value={inputValue} onChange={handleChange} className='autocomplete__input' />
+
         </div>
     );
 };
