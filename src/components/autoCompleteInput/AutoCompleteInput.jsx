73a0f657962cd1e2cd04c72c07c1977e9b12f234
Fernando <ing.fernandovg@gmail.com>
2024-06-08 12:32:29 -0600

    Comp_autocomplete step1

--- src/components/autoCompleteInput/AutoCompleteInput.jsx
+++ src/components/autoCompleteInput/AutoCompleteInput.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './autoCompleteInput.css';
 import useAutoComplete from '../../hooks/useAutoComplete';
 
 const AutoCompleteInput = ({suggestions, onSelect}) => {
 
-   
+    const [inputValue, setInputValue] = useState('');
+    const [filterSuggestions, setFilterSuggestions] = useState([]);
+    return (
+        <div className="autocomplete">
+            
+        </div>
+    );
 };
 
 export default AutoCompleteInput
